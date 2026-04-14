d05408155d666f9a76bb28918c906905a93440f2
Maksym Zhmura <zmuramaksim@gmail.com>
2026-04-14 21:44:02 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -74,5 +74,5 @@ class Slide:
         try:
             self.limitation_class(visitor.age, visitor.weight, visitor.height)
             return True
-        except (TypeError, ValueError):
+        except Exception:
             return False
